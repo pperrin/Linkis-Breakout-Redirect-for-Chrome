617ef83f8d3894eecdeefc36bb2a88dfd82d1eff
Paul Perrin <paul@idltd.com>
2016-09-29 21:26:52 +0100

    Fixed redirection forfor youtube links.

--- content.js
+++ content.js
@@ -8,6 +8,16 @@ var i;
 	if (link[i].getAttribute('rel') == 'canonical' )
 	{
 		newurl = link[i].getAttribute('href').replace('/linkis.com','');
+		if (newurl.search("youtube.")!=-1) {
+			link = window.document.getElementsByTagName('meta');
+			for (i = 0; i < link.length;   i++ )
+			{
+				if (link[i].getAttribute('name') == 'twitter:url' )
+				{
+					newurl=link[i].getAttribute('content')
+				}
+			}
+		}
 		chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
 		exit;
 	}
